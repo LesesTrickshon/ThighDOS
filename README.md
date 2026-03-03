4fa479d6a02e04843b4530d50a2942aafe6992eb
LeafTea <leaftide@proton.me>
2026-03-03 18:27:20 +0100

    Added Styling, Icons and README image

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 <h1 align="center">ThighDOS</h1>
 <p align="center">Hallo Herr Schlehan :D</p>
-![Concept](/src/assets/thighdos%20concept.png)
+![Concept](./src/assets/thighdos%20concept.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+// Functionalitys Imports:
 import "./App.css";
+
+// Icon imports:
+import { FaXmark } from "react-icons/fa6";
 
 function App() {
   const splash = "Nenn es ThighDOS ~Tobias Rieger 2026";
   return (
     <>
-      <div className="Top">
+      <div className="top">
         <h1>ThighDOS</h1>
         <p style={{ fontStyle: "italic" }}>{splash}</p>
+      </div>
+
+      <div className="warning">
+        <p>By using this software you (the user) are viable for any damages!</p>
+        <button><FaXmark /></button>
       </div>
     </>
   );
